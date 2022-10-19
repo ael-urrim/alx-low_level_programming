a226fe88d485fdff9bffbaee0bc11669e184006d
Ifedolapo Samuel Akindahunsi <80631962+ael-urrim@users.noreply.github.com>
2022-10-19 10:57:28 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * jack_bauer - A function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - A function that prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59
  */
+
 void jack_bauer(void)
 {
 	int i, j;
