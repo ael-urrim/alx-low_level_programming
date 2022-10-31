adcf4e9c26b20d6e3f312e94c6f9fb3fe08542be
Ifedolapo Samuel Akindahunsi <80631962+ael-urrim@users.noreply.github.com>
2022-10-31 08:14:04 +0100

    ALX - A function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memcpy - function that copies memory area
+ * *_memcpy - a function that copies memory area
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
